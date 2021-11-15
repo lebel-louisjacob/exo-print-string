1be7ef00ee45cdbead73010a792ff36af9409bb0
Louis-Jacob <lebel.louisjacob@gmail.com>
2021-11-15 13:12:22 +0000

    api 0.2.2

--- src/printCharacter.cpp
+++ src/printCharacter.cpp
@@ -2,31 +2,31 @@
 
 #include <cstdio>
 
-exo::Object function_property(exo::Object character_remainder, exo::Context const context) {
-    auto const&& bit_library { exo::find_parent("bit", context) };
-    auto const&& true_bit { exo::get_property("true", bit_library) };
+exo::Object function_property(exo::Object character_remainder, exo::Object const self) {
+    auto const&& bit_library { exo::find("bit", self) };
+    auto const&& true_bit { exo::get("true", bit_library) };
 
     uint32_t result { 0 };
 
     for (uint8_t i { 0 }; i < (sizeof(result) * 8); ++i) {
-        auto const&& character_is_zero { exo::get_property("isZero", character_remainder) };
+        auto const&& character_is_zero { exo::get("isZero", character_remainder) };
         if (character_is_zero == true_bit) break;
 
-        auto const&& character_is_odd { exo::get_property("isOdd", character_remainder) };
+        auto const&& character_is_odd { exo::get("isOdd", character_remainder) };
 
         result |= static_cast<uint32_t>(character_is_odd == true_bit) << i;
-        character_remainder = exo::get_property("shiftRight", character_remainder);
+        character_remainder = exo::get("shiftRight", character_remainder);
     }
 
     putchar(result);
 
-    auto const&& empty_library { exo::find_parent("empty", context) };
+    auto const&& empty_library { exo::find("empty", self) };
 
-    auto const&& promise_library { exo::find_parent("promise", context) };
-    auto const&& success_constructor { exo::get_property("success", promise_library) };
+    auto const&& promise_library { exo::find("promise", self) };
+    auto const&& success_constructor { exo::get("success", promise_library) };
     return exo::call(empty_library, success_constructor);
 }
 
-extern "C" void create_native_object(exo::ConstructionContext const construction_context, exo::Context const context) {
-    exo::define_function_property(&function_property, construction_context);
+extern "C" void create_native_object(exo::Context const context, exo::Object const self) {
+    exo::define_function_property(&function_property, context);
 }
