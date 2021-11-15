580432730dcdc4d7e4cf9494325e6f3c170fb41b
Louis-Jacob <lebel.louisjacob@gmail.com>
2021-11-15 12:08:14 +0000

    update for 0.2.2

--- src/printCharacter.cpp
+++ src/printCharacter.cpp
@@ -1,22 +1,21 @@
 #include <native.hpp>
 
 #include <cstdio>
-#include <algorithm>
 
 exo::Object function_property(exo::Object character_remainder, exo::Context const context) {
     auto const&& bit_library { exo::find_parent("bit", context) };
-    auto const&& true_bit { exo::get_property("true", bit_library, context) };
+    auto const&& true_bit { exo::get_property("true", bit_library) };
 
     uint32_t result { 0 };
 
     for (uint8_t i { 0 }; i < (sizeof(result) * 8); ++i) {
-        auto const&& character_is_zero { exo::get_property("isZero", character_remainder, context) };
+        auto const&& character_is_zero { exo::get_property("isZero", character_remainder) };
         if (character_is_zero == true_bit) break;
 
-        auto const&& character_is_odd { exo::get_property("isOdd", character_remainder, context) };
+        auto const&& character_is_odd { exo::get_property("isOdd", character_remainder) };
 
         result |= static_cast<uint32_t>(character_is_odd == true_bit) << i;
-        character_remainder = exo::get_property("shiftRight", character_remainder, context);
+        character_remainder = exo::get_property("shiftRight", character_remainder);
     }
 
     putchar(result);
@@ -24,8 +23,8 @@ exo::Object function_property(exo::Object character_remainder, exo::Context cons
     auto const&& empty_library { exo::find_parent("empty", context) };
 
     auto const&& promise_library { exo::find_parent("promise", context) };
-    auto const&& success_constructor { exo::get_property("success", promise_library, context) };
-    return exo::call(empty_library, success_constructor, context);
+    auto const&& success_constructor { exo::get_property("success", promise_library) };
+    return exo::call(empty_library, success_constructor);
 }
 
 extern "C" void create_native_object(exo::ConstructionContext const construction_context, exo::Context const context) {
